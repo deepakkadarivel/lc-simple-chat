7a8cb4dcb2deb5733ddded164b2644dc3ecbb888
Deepak <deepak.kadarivelu@conpend.com>
2025-01-14 13:55:15 +0530

    Ollama simple chat setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 console.log("Hello, world!");
+
+import { ChatOllama } from "@langchain/ollama";
+
+const llm = new ChatOllama({
+  model: "mario",
+  temperature: 1,
+  maxRetries: 2,
+});
+
+const aiMsg = await llm.invoke([
+  ["system", "Translate English to Dutch."],
+  ["human", "Hi, I'm a man."],
+]);
+aiMsg;
+
+console.log(aiMsg.content);
