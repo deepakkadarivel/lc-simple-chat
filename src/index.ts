54561e38ee3a8128f7bfc4ff27ee85c409530acb
Deepak <deepak.kadarivelu@conpend.com>
2025-01-23 13:57:50 +0530

    prompt chaining

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-console.log("Hello, world!");
-
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatOllama } from "@langchain/ollama";
 
 const llm = new ChatOllama({
   model: "mario",
   temperature: 1,
   maxRetries: 2,
+  format: "json",
 });
 
-const aiMsg = await llm.invoke([
-  ["system", "Translate English to Dutch."],
-  ["human", "Hi, I'm a man."],
+const prompt = ChatPromptTemplate.fromMessages([
+  [
+    "system",
+    "You are a helpful assistant that translates {input_language} to {output_language}",
+  ],
+  ["human", "{input}"],
 ]);
-aiMsg;
 
-console.log(aiMsg.content);
+const chain = prompt.pipe(llm);
+
+const output = await chain.invoke({
+  input_language: "English",
+  output_language: "Dutch",
+  input: "I love programming.",
+});
+
+console.log(output.content);
